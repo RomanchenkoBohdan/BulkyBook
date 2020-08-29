3025a39c324dff495bc02b4530b3646aa62846ca
Bohdan Romanchenko <rov4ak3@gmail.com>
2020-08-29 21:16:14 +0300

    Build Custom Tag Helper

--- BulkyBook.Models/PagingInfo.cs
+++ BulkyBook.Models/PagingInfo.cs
@@ -11,7 +11,7 @@ namespace BulkyBook.Models
         public int TotalItem { get; set; }
         public int ItemsPerPage { get; set; }
         public int CurrentPage { get; set; }
-        public int urlParam { get; set; }
+        public string urlParam { get; set; }
 
         public int TotalPage => (int)Math.Ceiling((decimal)TotalItem / ItemsPerPage);
     }
